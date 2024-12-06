63be79f7fe3c6036451da3e381b0c212867e39ad
mindex <dev@mindex.com>
2024-12-06 16:45:08 -0500

    added solution for task 2 compensation. Tests will be added next

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,35 @@
+package com.mindex.challenge.data;
+
+public class Compensation {
+    //We will connect to an employee via ID, instead of a full Employee object
+    private String employeeId;
+    private int salary;
+    private String effectiveDate;
+
+    public Compensation() {
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(String effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+}
